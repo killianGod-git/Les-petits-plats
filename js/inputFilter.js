4e3900750b1d5f5e302332bf2a5e366e10ef27ba
Killian Godmez <kgodmez@gmail.com>
2023-07-31 22:08:45 +0200

    style dropdown , tags + input

--- js/inputFilter.js
+++ js/inputFilter.js
@@ -10,44 +10,26 @@ export const selectedTags=[]
 function searchRecipes(searchTerm){
     const partialResult=selectedTags.length<=0? recipes:[]
     selectedTags.forEach(({type, tag})=>{
-        const list=tagsFilterRecipes(tag, type)
-        partialResult.push(list)
+        const list=tagsFilterRecipes(tag, type, false)
+        list.forEach((item) => {
+            if ( !partialResult.find((re) => re.id === item.id)){
+                partialResult.push(list)
+            }
+        })
     })
-    return partialResult.filter(recipes => {
-        recipes.forEach((recipe ) => {
-        let ingredientSearch = recipe.ingredients
-        console.log(ingredientSearch);
+    return partialResult.filter(recipe => {
         return recipe.name.toLowerCase().includes(searchTerm) ||
             recipe.description.toLowerCase().includes( searchTerm) ||
             recipe.ingredients.some((ingredientObj) => {
                 const ingredient = ingredientObj.ingredient.toLowerCase();
                 return ingredient.includes(searchTerm);
-          });  //ajouter include ingredients dans la recherche
-})})
-}
-
-// function searchRecipes(searchTerm){
-//     const partialResult=selectedTags.length<=0? recipes:[]
-//     selectedTags.forEach(({type, tag})=>{
-//         const list=tagsFilterRecipes(tag, type)
-//         partialResult.push(list)
-//     })
-//     return partialResult.filter(recipe => {
-        
-//         let ingredientSearch = recipe.ingredients
-//         console.log(ingredientSearch);
-//         return recipe.name.toLowerCase().includes(searchTerm) ||
-//             recipe.description.toLowerCase().includes( searchTerm) ||
-//             recipe.ingredients.some((ingredientObj) => {
-//                 const ingredient = ingredientObj.ingredient.toLowerCase();
-//                 return ingredient.includes(searchTerm);
-//         });  //ajouter include ingredients dans la recherche
-// })}
+        }); 
+})}
 function filterRecipes(){
     const searchTerm = mainSearch.value;
     const searchedRecipes = searchRecipes(searchTerm.toLowerCase());
     if (searchedRecipes.length !== 0){
-        console.log("recettes trouvées")
+        console.log(searchedRecipes)
         displayRecipe(searchedRecipes)
     } else {
         console.log("pas de recettes ")
